1fe5a06540827e5d422590c9286841df85f2e753
Aarón <aaronalejandro2@gmail.com>
2026-01-27 09:54:42 +0100

    Primer commit con servicio login

---
+++ ParkVault/MainWindow.xaml.cs
@@ -0,0 +1,432 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ParkVault
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int selectedBoxRow;
+        int selectedBoxColumn;
+        enum OpCode
+        {
+            register,
+            unregister,
+            login,
+            logout
+        };
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Loaded += MainWindow_Loaded;
+        }
+
+        // A partir de un puerto, establecemos conexión con el servidor y devolvemos el socket creado para poder seguir trabajando con él
+        private Socket CreateSocketAndConnectServer(int port)
+        {
+            IPAddress address = IPAddress.Parse(ServerText.Text);
+            IPEndPoint endPoint = new IPEndPoint(address, port);
+            Socket socket = new Socket(address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            socket.Connect(endPoint);
+            
+            return socket;
+        }
+
+        private int userPort = 1000;
+        private int boxPort = 1001;
+
+        // A partir de un string y un socket, enviamos por este la cantidad de bytes a enviar y luego estos en cuestión
+        private void SendString(string s, Socket socket)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(s);
+            int count = bytes.Length;
+            byte[] bytesCount = BitConverter.GetBytes(count);
+            socket.Send(bytesCount);
+            socket.Send(bytes);
+        }
+
+        // Envía un int a través de un socket
+        private void SendInt(int i, Socket socket)
+        {
+            byte[] bytes = BitConverter.GetBytes(i);
+            socket.Send(bytes);
+        }
+
+        // Recibe bytes desde un socket y devuelve el int resultante
+        static int ReceiveInt(Socket socket)
+        {
+            byte[] bytes = new byte[sizeof(int)];
+            socket.Receive(bytes);
+            int numRecibido = BitConverter.ToInt32(bytes);
+            
+            return numRecibido;
+        }
+
+        private void MainWindow_Loaded(object sender,RoutedEventArgs e)
+        {
+            ServerText.Text = "127.0.0.1";
+            UserNameText.Text = "";
+            PasswordText.Text = "";
+            
+            SelectedBoxText.Text = "None";
+            BoxStatusText.Text = "Unknown";
+
+            AccessCodeText.Text = "";
+
+            selectedBoxRow = -1;
+            selectedBoxColumn = -1;
+
+            OperatePanel.Visibility = Visibility.Visible;
+            WaitPanel.Visibility = Visibility.Hidden;
+        }
+
+        private void Window_MouseDown(object sender,MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                DragMove();
+            }
+        }
+
+        private void LoginButton_Click(object sender,RoutedEventArgs e)
+        {
+            //////////////////////////////
+            // Loguearse en el servidor //
+            //////////////////////////////
+            
+            Socket socket = CreateSocketAndConnectServer(userPort);
+
+            // Primero enviamos el num que determina la orden que va a recibir el servidor
+            
+            int code = (int)OpCode.login;
+            SendInt(code, socket);
+
+            // Hacemos dos envíos, uno de username y otro de password
+            SendString(UserNameText.Text, socket);
+            SendString(PasswordText.Text, socket);
+
+            // Recibimos respuesta del server y la mostramos
+            int response = ReceiveInt(socket);
+
+            if (response == 0)
+            {
+                MessageBox.Show("OK");
+            }
+            else if (response == -1)
+            {
+                MessageBox.Show("Password incorrecto");
+            }
+            else if(response == -2)
+            {
+                MessageBox.Show("Usuario no registrado");
+            }
+            else if (response == -3)
+            {
+                MessageBox.Show("Usuario ya logueado");
+            }
+
+        }
+
+        private void LogoutButton_Click(object sender,RoutedEventArgs e)
+        {
+            /////////////////////////////////
+            // Desloguearse en el servidor //
+            /////////////////////////////////
+
+            Socket socket = CreateSocketAndConnectServer(userPort);
+
+            // Primero enviamos el num que determina la orden que va a recibir el servidor
+
+            int code = (int)OpCode.logout;
+            SendInt(code, socket);
+
+            // Hacemos dos envíos, uno de username y otro de password
+            SendString(UserNameText.Text, socket);
+            SendString(PasswordText.Text, socket);
+
+            // Recibimos respuesta del server y la mostramos
+            int response = ReceiveInt(socket);
+
+            if (response == 0)
+            {
+                MessageBox.Show("OK");
+            }
+            else if (response == -1)
+            {
+                MessageBox.Show("Password incorrecto");
+            }
+            else if (response == -2)
+            {
+                MessageBox.Show("Usuario no logueado");
+            }
+            else if (response == -3)
+            {
+                MessageBox.Show("Usuario no registrado");
+            }
+        }
+
+        private void RegisterButton_Click(object sender,RoutedEventArgs e)
+        {
+            ////////////////////////////////
+            // Registrarse en el servidor //
+            ////////////////////////////////
+
+            Socket socket = CreateSocketAndConnectServer(userPort);
+
+            // Primero enviamos el num que determina la orden que va a recibir el servidor
+
+            int code = (int)OpCode.register;
+            SendInt(code, socket);
+
+            // Hacemos dos envíos, uno de username y otro de password
+            SendString(UserNameText.Text, socket);
+            SendString(PasswordText.Text, socket);
+
+            // Recibimos respuesta del server y la mostramos
+            int response = ReceiveInt(socket);
+
+            if (response == 0)
+            {
+                MessageBox.Show("OK");
+            }
+            else if (response == -1)
+            {
+                MessageBox.Show("Password incorrecto, mínimo 8 caracteres");
+            }            
+            else if (response == -2)
+            {
+                MessageBox.Show("Usuario ya registrado");
+            }
+        }
+
+        private void UnregisterButton_Click(object sender,RoutedEventArgs e)
+        {
+            ///////////////////////////////////
+            // Desregistrarse en el servidor //
+            ///////////////////////////////////
+
+            Socket socket = CreateSocketAndConnectServer(userPort);
+
+            // Primero enviamos el num que determina la orden que va a recibir el servidor
+
+            int code = (int)OpCode.unregister;
+            SendInt(code, socket);
+
+            // Hacemos dos envíos, uno de username y otro de password
+            SendString(UserNameText.Text, socket);
+            SendString(PasswordText.Text, socket);
+
+            // Recibimos respuesta del server y la mostramos
+            int response = ReceiveInt(socket);
+
+            if (response == 0)
+            {
+                MessageBox.Show("OK");
+            }
+            else if (response == -1)
+            {
+                MessageBox.Show("Password incorrecto");
+            }
+            else if (response == -2)
+            {
+                MessageBox.Show("Usuario no registrado");
+            }
+        }
+
+        private void GetContentsButton_Click(object sender,RoutedEventArgs e)
+        {
+            ////////////////////////////////////////////
+            // Intentar obtener el fichero de la caja //
+            ////////////////////////////////////////////
+
+            Socket socket = CreateSocketAndConnectServer(boxPort);
+        }
+
+        private void PutContentsButton_Click(object sender,RoutedEventArgs e)
+        {
+            ////////////////////////////////////////////
+            // Intentar poner un fichero en la caja   //
+            ////////////////////////////////////////////
+
+            Socket socket = CreateSocketAndConnectServer(boxPort);
+        }
+
+        private void ClearContentsButton_Click(object sender,RoutedEventArgs e)
+        {
+            ///////////////////////////////////////////////
+            // Intentar eliminar el contenido de la caja //
+            ///////////////////////////////////////////////
+
+            Socket socket = CreateSocketAndConnectServer(boxPort);
+        }
+
+        private void ShowWaitPanel()
+        {
+            OperatePanel.Visibility = Visibility.Hidden;
+            WaitPanel.Visibility = Visibility.Visible;
+        }
+
+        private void HideWaitPanel()
+        {
+            OperatePanel.Visibility = Visibility.Visible;
+            WaitPanel.Visibility = Visibility.Hidden;
+        }
+
+        private void Box0AButton_Click(object sender,RoutedEventArgs e)
+        {
+            SelectedBoxText.Text = "0A";
+
+            selectedBoxRow = 0;
+            selectedBoxColumn = 0;
+
+        }
+
+        private void Box0BButton_Click(object sender,RoutedEventArgs e)
+        {
+            SelectedBoxText.Text = "0B";
+
+            selectedBoxRow = 0;
+            selectedBoxColumn = 1;
+        }
+
+        private void Box0CButton_Click(object sender,RoutedEventArgs e)
+        {
+            SelectedBoxText.Text = "0C";
+
+            selectedBoxRow = 0;
+            selectedBoxColumn = 2;
+        }
+
+        private void Box0DButton_Click(object sender,RoutedEventArgs e)
+        {
+            SelectedBoxText.Text = "0D";
+
+            selectedBoxRow = 0;
+            selectedBoxColumn = 3;
+        }
+
+        private void Box1AButton_Click(object sender,RoutedEventArgs e)
+        {
+            SelectedBoxText.Text = "1A";
+
+            selectedBoxRow = 1;
+            selectedBoxColumn = 0;
+        }
+
+        private void Box1BButton_Click(object sender,RoutedEventArgs e)
+        {
+            SelectedBoxText.Text = "1B";
+
+            selectedBoxRow = 1;
+            selectedBoxColumn = 1;
+
+        }
+
+        private void Box1CButton_Click(object sender,RoutedEventArgs e)
+        {
+            SelectedBoxText.Text = "1C";
+
+            selectedBoxRow = 1;
+            selectedBoxColumn = 2;
+
+        }
+
+        private void Box1DButton_Click(object sender,RoutedEventArgs e)
+        {
+            SelectedBoxText.Text = "1D";
+
+            selectedBoxRow = 1;
+            selectedBoxColumn = 3;
+
+        }
+
+        private void Box2AButton_Click(object sender,RoutedEventArgs e)
+        {
+            SelectedBoxText.Text = "2A";
+
+            selectedBoxRow = 2;
+            selectedBoxColumn = 0;
+
+        }
+
+        private void Box2BButton_Click(object sender,RoutedEventArgs e)
+        {
+            SelectedBoxText.Text = "2B";
+
+            selectedBoxRow = 2;
+            selectedBoxColumn = 1;
+
+        }
+
+        private void Box2CButton_Click(object sender,RoutedEventArgs e)
+        {
+            SelectedBoxText.Text = "2C";
+
+            selectedBoxRow = 2;
+            selectedBoxColumn = 2;
+
+        }
+
+        private void Box2DButton_Click(object sender,RoutedEventArgs e)
+        {
+            SelectedBoxText.Text = "2D";
+
+            selectedBoxRow = 2;
+            selectedBoxColumn = 3;
+
+        }
+
+        private void Box3AButton_Click(object sender,RoutedEventArgs e)
+        {
+            SelectedBoxText.Text = "3A";
+
+            selectedBoxRow = 3;
+            selectedBoxColumn = 0;
+
+        }
+
+        private void Box3BButton_Click(object sender,RoutedEventArgs e)
+        {
+            SelectedBoxText.Text = "3B";
+
+            selectedBoxRow = 3;
+            selectedBoxColumn = 1;
+
+        }
+
+        private void Box3CButton_Click(object sender,RoutedEventArgs e)
+        {
+            SelectedBoxText.Text = "3C";
+
+            selectedBoxRow = 3;
+            selectedBoxColumn = 2;
+
+        }
+
+        private void Box3DButton_Click(object sender,RoutedEventArgs e)
+        {
+            SelectedBoxText.Text = "3D";
+
+            selectedBoxRow = 3;
+            selectedBoxColumn = 3;
+
+        }
+        
+    }
+}
---
+++ ParkVaultServer/Program.cs
@@ -0,0 +1,237 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace ParkVaultServer
+{
+    //public class LoginOperation
+    //{
+    //    Socket socket;
+    //    public LoginOperation(Socket s)
+    //    {
+    //        socket = s;
+    //    }
+
+    //    public void Execute()
+    //    {
+    //        //...
+    //    }
+
+    //}
+
+
+    //LoginOperation o = new LoginOperation(socket);
+    //Thread thread = new Thread(o.Execute);
+
+
+    internal class Program
+    {
+        
+        static Dictionary<string, string> registeredUsers = new Dictionary<string, string>();
+        static List<string> loggedUsers = new List<string>();
+
+        static void UserServiceServer()
+        {
+            // Login de clientes
+            IPAddress address = IPAddress.Parse("127.0.0.1");
+            IPEndPoint loginEndPoint = new IPEndPoint(address, 1000);
+            Socket loginServerSocket = new Socket(address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            Socket loginServiceSocket;
+
+            loginServerSocket.Bind(loginEndPoint);
+            loginServerSocket.Listen();
+
+            while (loginServerSocket.IsBound)
+            {
+                loginServiceSocket = loginServerSocket.Accept();
+
+                ParameterizedThreadStart threadStart = new ParameterizedThreadStart(UserService);
+                Thread userThread = new Thread(threadStart);
+                userThread.Start(loginServiceSocket);
+
+
+                //foreach (KeyValuePair<string, string> par in registeredUsers)
+                //{
+                //    Console.WriteLine($"Clave: {par.Key}, Valor: {par.Value}");
+                //}
+
+                //foreach (string user in loggedUsers)
+                //{
+                //    Console.WriteLine($"Usuario logeado: {user}");
+                //}
+            }
+        }
+
+        static void UserService(object o)
+        {
+            int order;
+            string user;
+            string password;
+
+            Socket loginServiceSocket = (Socket)o;  
+
+            // primero saber qué orden envía!!
+            order = ReceiveInt(loginServiceSocket);
+            // Recibimos valores de user y password
+            user = ReceiveString(loginServiceSocket);
+            password = ReceiveString(loginServiceSocket);
+
+            // comprobar si el usuario existe o no
+            bool userExists = UserExists(user);
+
+            //Thread.Sleep(1000);
+
+            if (order == 0) //register
+            {
+
+                if (userExists)
+                {
+                    SendInt(-2, loginServiceSocket);
+                }
+                else
+                {
+                    // comprobamos que no esté ya logueado
+                    if (password.Length < 8)
+                    {
+                        SendInt(-1, loginServiceSocket);
+                    }
+                    else
+                    {
+                        registeredUsers.Add(user, password);
+                        SendInt(0, loginServiceSocket);
+                    }
+                }
+
+            }
+            else if (order == 1) //unregister
+            {
+
+                if (!userExists)
+                {
+                    SendInt(-2, loginServiceSocket);
+                }
+                else // exists
+                {
+                    // comprobamos password
+                    if (registeredUsers[user] == password)
+                    {
+                        registeredUsers.Remove(user);
+                        SendInt(0, loginServiceSocket);
+                    }
+                    else
+                    {
+                        SendInt(-1, loginServiceSocket);
+                    }
+                }
+            }
+            else if (order == 2) //login
+            {
+
+                if (userExists)
+                {
+                    // comprobamos si ya está logeado
+                    if (loggedUsers.Contains(user))
+                    {
+                        SendInt(-3, loginServiceSocket);
+                    }
+                    else // existe y no está logeado
+                    {
+                        // comprobamos si pw es correcta
+                        if (registeredUsers[user] == password)
+                        {
+                            loggedUsers.Add(user);
+                            SendInt(0, loginServiceSocket);
+                        }
+                        else // pw incorrecta
+                        {
+                            SendInt(-1, loginServiceSocket);
+                        }
+                    }
+
+                }
+                else // no existe el usuario
+                {
+                    SendInt(-2, loginServiceSocket);
+                }
+            }
+            else // order 3 (logout)
+            {
+                if (userExists)
+                {
+                    // comprobamos si no está logeado
+                    if (!loggedUsers.Contains(user))
+                    {
+                        SendInt(-2, loginServiceSocket);
+                    }
+                    else // existe y está logeado
+                    {
+                        // comprobamos si pw es correcta
+                        if (registeredUsers[user] == password)
+                        {
+                            loggedUsers.Remove(user);
+                            SendInt(0, loginServiceSocket);
+                        }
+                        else // pw incorrecta
+                        {
+                            SendInt(-1, loginServiceSocket);
+                        }
+                    }
+
+                }
+                else // no existe el usuario
+                {
+                    SendInt(-3, loginServiceSocket);
+                }
+            }
+
+            loginServiceSocket.Close();
+        }
+        static string ReceiveString(Socket socket)
+        {
+            byte[] bytesCount = new byte[sizeof(int)];
+            socket.Receive(bytesCount);
+            int count = BitConverter.ToInt32(bytesCount);
+            byte[] bytes = new byte[count];
+            socket.Receive(bytes);
+            string s = Encoding.UTF8.GetString(bytes);
+            return s;
+        }
+
+        static int ReceiveInt(Socket socket)
+        {
+            byte[] bytes = new byte[sizeof(int)];
+            socket.Receive(bytes);
+            int numRecibido = BitConverter.ToInt32(bytes);
+
+            return numRecibido;
+        }
+
+        static void SendInt(int i, Socket socket)
+        {
+            byte[] bytes = BitConverter.GetBytes(i);
+            socket.Send(bytes);
+        }
+              
+        static bool UserExists (string user)
+        {
+            bool userExists = false;
+
+            if (registeredUsers.ContainsKey(user))
+            {
+                userExists = true;
+            }
+
+            return userExists;
+        }
+
+        static void Main(string[] args)
+        {
+
+            Thread userThread = new Thread(UserServiceServer);
+            userThread.Start(); 
+            
+
+        }
+    }
+}
